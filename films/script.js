8c6f146c673fc5bdb71f7402868605b032b70867
awaqum <bobbysukaanderson@gmail.com>
2022-07-04 13:29:20 +0300

    true films with cycles

--- films/script.js
+++ films/script.js
@@ -1,6 +1,16 @@
 'use strict';
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', "");
+if (numberOfFilms < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if ( numberOfFilms < 30) {
+    alert('Вы классический зритель');
+} else if (numberOfFilms > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,9 +20,18 @@ const personalMovieDB = {
     private: false
 };
 
-const lastFilm1 = prompt('Один из поледних просмотренных фильмов?', "");
-personalMovieDB.movies[lastFilm1] = prompt('На сколько цените его?', "");
-const lastFilm2 = prompt('Один из поледних просмотренных фильмов?', "");
-personalMovieDB.movies[lastFilm2] = prompt('На сколько цените его?', "");
+ for (let i=1; i <= numberOfFilms; i++) {
+    const lastFilm = prompt('Один из поледних просмотренных фильмов?', "");
+    if (lastFilm === '') {
+        i--;
+    } else if (lastFilm.length > 50) {
+        i--;
+    } else if (lastFilm === null) {
+        i--;
+    } else {
+        personalMovieDB.movies[lastFilm] = prompt('На сколько цените его?', "");
+    }
+   
+}
 
 // console.log(personalMovieDB);
